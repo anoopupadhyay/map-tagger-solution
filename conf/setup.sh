a9fc09d96c7fde38f56e7623dbc06f8b787f9fc0
snmatus <snmatus@amazon.com>
2024-05-30 21:57:42 +0000

    initial version

---
+++ conf/setup.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+
+
+#Verify code version
+version="$(curl https://version.code.ds.wwcs.aws.dev/?codeId=maptagger'&'moduleId=deploy)"
+
+#Install Software Packages
+# Bug : https://github.com/amazonlinux/amazon-linux-2023/issues/397
+while true; do
+    echo "Trying to install app rpm packages ..."
+    sudo yum install -y openssl nginx cronie && break
+done
+
+while true; do
+    echo "Trying to install db rpm packages ..."
+    sudo dnf install mariadb105-server -y && break
+done
+
+
+while true; do
+    echo "Trying to install agent rpm packages ..."
+    sudo yum install -y python3.11-pip && break
+done
+
+#Configure Agent Libraries
+pip3.11 install boto3
+pip3.11 install pymysql
+pip3.11 install cryptography
+ 
+ 
+#Change permissions
+sudo chown -R ec2-user:ec2-user /aws/apps
+
+
+#Configure database service
+sudo cp /etc/my.cnf /tmp/my.cnf
+sudo sh -c 'echo -e "\n[mysqld]\nskip-grant-tables\nskip-networking" >> /etc/my.cnf'
+sudo systemctl start mariadb
+sudo chkconfig mariadb on
+cd /aws/apps/conf/; mysql --socket=/var/lib/mysql/mysql.sock < database.sql
+
+#Configure database credentials
+uuid=$(uuidgen)
+mysql -e "FLUSH PRIVILEGES; CREATE USER 'app'@'localhost' IDENTIFIED BY '$uuid';GRANT ALL PRIVILEGES ON db.* TO 'app'@'localhost';"
+echo "{ \"user\" : \"app\", \"key\": \"$uuid\" }" > /aws/apps/server/credentials.json
+
+#Restart database dervices
+sudo cp /tmp/my.cnf /etc/my.cnf
+sudo service mariadb restart
+
+
+
+#Create Certificates
+sudo mkdir /etc/nginx/ssl/
+sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/nginx/ssl/server.key -out /etc/nginx/ssl/server.crt -subj "/C=US/ST=US/L=US/O=Global Security/OU=IT Department/CN=127.0.0.1"
+
+#Copy Configurations
+sudo cp /aws/apps/conf/api.core.service /usr/lib/systemd/system/api.core.service
+sudo cp /aws/apps/conf/server.conf /etc/nginx/conf.d/
+
+#Enable Auto-Start
+sudo chkconfig nginx on
+sudo chkconfig api.core on
+sudo chkconfig crond on
+
+
+#Copy aws-exports.json
+cp /aws/apps/conf/aws-exports.json /aws/apps/frontend/public/
+cp /aws/apps/conf/aws-exports.json /aws/apps/server/
+
+
+#Re-Start NGINX Services
+sudo service nginx restart
+
+#NodeJS Installation
+curl https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh --output install.sh
+sh install.sh
+. ~/.nvm/nvm.sh
+nvm install 20.11.0
+
+
+#NodeJS API Core Installation
+cd /aws/apps/server/; npm install;
+
+#Re-Start API Services
+sudo service api.core restart
+
+#React Application Installation
+cd /aws/apps/frontend/; npm install; npm run build;
+
+
+#Copy build content to www folder
+cp -r /aws/apps/frontend/build/* /aws/apps/frontend/www/
+
+
+#Agent scheduler
+sudo service crond restart
+crontab -l | { cat; echo "0 1 * * * sh /aws/apps/agent/run.sh"; } | crontab -
+
---
+++ server/class.tagger.js
@@ -0,0 +1,84 @@
+const exec = require('child_process').exec;
+const fs = require('fs');
+
+//--#############
+//--############# CLASS : classTaggerProcess
+//--#############
+
+
+class classTaggerProcess {
+
+        logging = [];
+        status = "non-started"
+        applicationDirectory = "/aws/apps/agent/";
+        scriptCommand = "sudo -u ec2-user sh /aws/apps/agent/run.sh"
+        constructor(object) { 
+            
+        }
+        
+        //-- StartUpdate
+        startProcess(module,type,message) { 
+            
+            this.status = "started";
+            this.logging = [];
+            const objectShell = exec(this.scriptCommand);
+            objectShell.stdout.on('data', (data)=>{
+                    if (data !== "") {
+                        this.logging.unshift({ timestamp : new Date().toLocaleString(), message : data });
+                        this.status = "in-progress";
+                    }
+            });
+            
+            objectShell.stderr.on('data', (data)=>{
+                    if (data !== "") {
+                        this.logging.unshift({ timestamp : new Date().toLocaleString(), message : data });
+                        this.status = "in-progress";
+                    }
+            });
+            
+            objectShell.on('close', (code) => {
+              this.status = "completed";
+            });
+            
+        }
+        
+        
+        //-- Get Update Status
+        getUpdateLog(){
+            
+            return this.logging;
+            
+        }
+        
+        
+        //-- Get log files
+        getLoggingFiles(){
+            var files = [];
+            try {
+                fs.readdirSync(this.applicationDirectory).forEach(file => {
+                  if (file.slice(-4) === ".log")
+                    files.push(file);
+                });
+            } catch (err) {
+                console.error(err);
+            }
+            return files.sort().reverse();
+        }
+        
+        //-- Read log fiile
+        getLogfileContent(fileName){
+            var logContent = [];
+            try {
+                logContent = fs.readFileSync(this.applicationDirectory + fileName, 'utf8').split('\n');
+            } catch (err) {
+                console.error(err);
+            }
+            
+            return logContent;
+            
+        }
+        
+        
+}
+
+module.exports = { classTaggerProcess };
